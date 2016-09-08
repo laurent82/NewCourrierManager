dc55e647fba274b3aeed6905d9618ca803af965b
Laurent.Engels <laurent.engels@medicim.com>
2016-09-08 17:26:45 +0200

    Ajout d'un stylesheet

--- source/CourrierManager/program/src/main.cpp
+++ source/CourrierManager/program/src/main.cpp
@@ -1,14 +1,26 @@
 #include <QtWidgets/QApplication>
+#include <QFile>
+#include <QDebug>
 #include "CControler.h"
-
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
 
     QCoreApplication::setOrganizationName("LaurentEngelsSuite");
     QCoreApplication::setApplicationName("CourrierManager");
 
+    // Loading stylesheet
+    QFile file_sheet(":/stylesheet.qss");
+    if ( file_sheet.open(QFile::ReadOnly) )
+    {
+        QString full_styleSheet = QLatin1String(file_sheet.readAll());
+        app.setStyleSheet( full_styleSheet );
+    }
+    else
+    {
+        qWarning() << "Stylesheet introuvable...";
+    }
     CControler w;
-    return a.exec();
+    return app.exec();
 }
